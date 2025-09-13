87cfa368613442a82344ce759d03eb7cb0aaf4c6
vitorlopes <vitorhllopes18@gmail.com>
2025-09-13 00:30:30 -0300

    feat: Implementa configuração inicial do Spring Security
    - Configura endpoints públicos e privados para a API.
    
    - Adiciona gerenciamento de usuários com senhas criptografadas usando BCrypt.

---
+++ ecommerce/src/main/java/br/com/vitorlopes/ecommerce/security/MyECFilter.java
@@ -0,0 +1,18 @@
+package br.com.vitorlopes.ecommerce.security;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class MyECFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //encaminhando a requisição
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ ecommerce/src/main/java/br/com/vitorlopes/ecommerce/security/MySecurityConfig.java
@@ -0,0 +1,36 @@
+package br.com.vitorlopes.ecommerce.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // 1. Configuração do CSRF usando uma lambda
+                .csrf(csrf -> csrf.disable())
+
+                // 2. Configuração das regras de autorização usando uma lambda
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/produtos").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(withDefaults());
+
+        http.addFilterBefore(new MyECFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
